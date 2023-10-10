b34dd1f992ffc5457e8493e64c0c602fc5261ad8
Satpal Yadav <satpal.yadav@wellthy.care>
2023-10-10 21:33:27 +0530

    Initial commit

---
+++ app/src/main/java/satpal/imageprocessing/ImageProcessingApp.java
@@ -0,0 +1,10 @@
+package satpal.imageprocessing;
+
+import android.app.Application;
+
+public class ImageProcessingApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
